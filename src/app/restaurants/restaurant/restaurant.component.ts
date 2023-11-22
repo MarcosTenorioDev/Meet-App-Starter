f63db66bf5549f65f46b5c67d28f32bfa0c193d2
Marco Tenorio <marco.tenorio@direction.com.br>
2023-11-22 14:13:47 -0300

    :bulb: learning about routes, components, directives etc in angular 4

---
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { Restaurant } from './restaurant.model';
+
+@Component({
+  selector: 'mt-restaurant',
+  templateUrl: './restaurant.component.html',
+})
+export class RestaurantComponent implements OnInit {
+
+  /* A classe do componente, que recebe a interface
+  como propriedade a ser utilizada no template html */
+  @Input() restaurant: Restaurant 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
